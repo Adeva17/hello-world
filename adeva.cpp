11ac5add07eecb97538c5029ab71d6508579ab64
Muhammad Adeva <mhmmdadeva@gmail.com>
2024-03-17 23:03:55 +0700

    Project baru

---
+++ adeva.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+ 
+#define bismillah ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+ 
+using namespace std;
+ 
+ 
+int main(){
+    bismillah;
+    cout << ">>>100 FAKTA<<<";
+    for(int i = 0; i<100; i++){
+        cout << i+1 << ". Kimberlie orangnya sangat baik dan lucu :3" << endl;
+    }
+    return 0;
+}
